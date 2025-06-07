a2919fb85aa86bb25a6fb516fb1d24c59631b40a
weideming <wdmcoding0620@163.com>
2025-06-07 11:09:55 +0800

    微调

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import ElementPlus from 'unplugin-element-plus/vite'
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [vue(),ElementPlus()],
   resolve: {
     alias: {
